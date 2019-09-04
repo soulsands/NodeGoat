741f100b1853170ee9b113274b76d829070312ff
sandz <407221377@qq.com>
2019-09-04 18:17:30 +0800

    调整了,可以在家使用

--- app/routes/allocations.js
+++ app/routes/allocations.js
@@ -1,27 +1,31 @@
 var AllocationsDAO = require("../data/allocations-dao").AllocationsDAO;
 
 function AllocationsHandler(db) {
-    "use strict";
+  "use strict";
 
-    var allocationsDAO = new AllocationsDAO(db);
+  var allocationsDAO = new AllocationsDAO(db);
 
-
-    this.displayAllocations = function(req, res, next) {
-        /*
+  this.displayAllocations = function(req, res, next) {
+    /*
         // Fix for A4 Insecure DOR -  take user id from session instead of from URL param
         var userId = req.session.userId;
         */
-        var userId = req.params.userId;
+    var userId = req.params.userId;
+    console.log(userId);
 
-        allocationsDAO.getByUserIdAndThreshold(userId, req.query.threshold, function(err, allocations) {
-            if (err) return next(err);
+    allocationsDAO.getByUserIdAndThreshold(
+      userId,
+      req.query.threshold,
+      function(err, allocations) {
+        if (err) return next(err);
 
-            return res.render("allocations", {
-                userId: userId,
-                allocations: allocations
-            });
+        return res.render("allocations", {
+          userId: userId,
+          allocations: allocations
         });
-    };
+      }
+    );
+  };
 }
 
 module.exports = AllocationsHandler;
